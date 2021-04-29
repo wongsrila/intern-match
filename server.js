c7c5750ff6bb91fc66eb4819fc173791cb0e1889
wongsrila <jiraphong.wongsrila@gmail.com>
2021-04-29 21:51:41 +0200

    naam van database veranderd

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 dotenv.config();
 
 // database connection
-const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@internmatch.bgqjg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const dbURI = `mongodb+srv://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@internmatch.bgqjg.mongodb.net/intern-match?retryWrites=true&w=majority`;
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then((result) => app.listen(3000))
     .catch((err) => console.log(err));
